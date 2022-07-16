24421bdbf49fb9c1395a6643ec77e1b08fefcb58
Herimino Rakotondrafara <heriminorakotondrafara@Heriminos-MacBook-Pro.local>
2022-07-16 18:05:16 -0400

    offshored cleaning

--- Optimizeall.py
+++ Optimizeall.py
@@ -7,10 +7,6 @@ import math
 
 nv = 50
 
-def exposantTonumerique(df:pd.DataFrame):
-    df = df.apply(pd.to_numeric, errors='coerce')
-    return df
-
 def cleaner(data:pd.DataFrame):
     df= data[data['Vds'] != 0]
     return df
@@ -24,12 +20,5 @@ Vg1min=min(IdVg1['Ids'])
 
 Vv=pd.DataFrame(columns=['one','two','three','four','five'])
 Id=pd.DataFrame(columns=['one','two','three','four','five'])
-Vv['one']=np.linspace(Vg1min,Vg1max,nv+1)#add plus one to get 50 elements
-#Id['one'] = math.log10(np.interp(IdVg1['Ids'],Vv['one'],IdVg1['Vds']))
-
-
-
-resp=exposantTonumerique(data.data2Output30V())
-
-
-print(resp)
+Vv['one']=np.linspace(Vg1min,Vg1max,nv+1) #add plus one to get 50 elements
+Id['one'] = math.log10(np.interp(IdVg1['Ids'],Vv['one'],IdVg1['Vds']))
